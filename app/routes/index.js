e991a52f69da5a12f0dd337ab846fb2d4bbf4a42
kalindimchev <kalin.dimchev@gmail.com>
2017-09-14 00:37:14 +0300

    handling a 404

--- app/routes/index.js
+++ app/routes/index.js
@@ -20,7 +20,10 @@ module.exports = () => {
         },
         'post': {
 
-        }        
+        },
+        'NA': (req, res, next) => {
+            res.status(404).sendFile(process.cwd() + '/views/404.htm');
+        }         
     }
 
     return helper.route(routes);
