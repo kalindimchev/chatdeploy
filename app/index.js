da3714831305bc40e8db07c3632854240e3c68e1
kalindimchev <kalin.dimchev@gmail.com>
2017-09-13 19:09:27 +0300

    fix

--- app/index.js
+++ app/index.js
@@ -1,5 +1,11 @@
 'user strict'
 
+const router = require('express').Router();
+
+router.get('/', (req, res, next) => {
+    res.render('login');
+});
+
 module.exports = {
     router: require('./routes')()
 }
