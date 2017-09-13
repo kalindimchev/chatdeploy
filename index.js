2700b70d399771fab653b506a464bec1d13b9f88
kalindimchev <kalin.dimchev@gmail.com>
2017-09-13 17:06:18 +0300

    Change

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var app = express();
 var port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('<h1> Test Heroku Deploy </h1>');
+    res.send('<h1> Test Heroku Deploy #363 </h1>');
 })
 
 app.listen(port);
