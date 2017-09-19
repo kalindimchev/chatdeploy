6cdb683905f3b44bfe31ad68a5a24b604b3d3503
kalindimchev <kalin.dimchev@gmail.com>
2017-09-19 10:54:33 +0300

    add twitter auth

--- app/auth/index.js
+++ app/auth/index.js
@@ -3,6 +3,7 @@
 const passport = require('passport');
 const config = require('../config');
 const FacebookStrategy = require('passport-facebook').Strategy;
+const TwitterStrategy = require('passport-twitter').Strategy;
 const helper = require('../helpers');
 
 module.exports = () => {
@@ -33,4 +34,5 @@ module.exports = () => {
     }
 
     passport.use(new FacebookStrategy(config.fb, authProcessor));
+    passport.use(new TwitterStrategy(config.twitter, authProcessor));
 }
