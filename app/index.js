f58dfb8550331affc9fd4954aecdc989eb40fe68
kalindimchev <kalin.dimchev@gmail.com>
2017-09-20 02:40:55 +0300

    fix

--- app/index.js
+++ app/index.js
@@ -4,18 +4,18 @@
 require('./auth')();
 
 //Create IO Server instance
-let ioServer = app => {
-    return app;
+// let ioServer = app => {
+//     return app;
 
-    // app.locals.chatrooms = [];
-    // const server = require('http').Server(app);
-    // const io = require('socket.io')(server);
-    // io.use((socket, next) => {
-    //     require('./session')(socket.request, {}, next);
-    // });
-    // require('./socket')(io, app);
-    // return server;
-}
+//     // app.locals.chatrooms = [];
+//     // const server = require('http').Server(app);
+//     // const io = require('socket.io')(server);
+//     // io.use((socket, next) => {
+//     //     require('./session')(socket.request, {}, next);
+//     // });
+//     // require('./socket')(io, app);
+//     // return server;
+// }
 
 module.exports = {
     router: require('./routes')(),
