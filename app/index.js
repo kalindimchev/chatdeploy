f024d97092ecae8b4933345fc0f716f35fbe69ed
kalindimchev <kalin.dimchev@gmail.com>
2017-09-20 02:16:42 +0300

    fix

--- app/index.js
+++ app/index.js
@@ -5,14 +5,15 @@ require('./auth')();
 
 //Create IO Server instance
 let ioServer = app => {
-    app.locals.chatrooms = [];
-    const server = require('http').Server(app);
+    return app
+
+    // app.locals.chatrooms = [];
+    // const server = require('http').Server(app);
     // const io = require('socket.io')(server);
     // io.use((socket, next) => {
     //     require('./session')(socket.request, {}, next);
     // });
     // require('./socket')(io, app);
-    return app
     // return server;
 }
 
