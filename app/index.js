734a351035596756e41408a866d04f6c9e17967d
kalindimchev <kalin.dimchev@gmail.com>
2017-09-21 14:38:56 +0300

    try fix socket.io

--- app/index.js
+++ app/index.js
@@ -8,7 +8,7 @@ let ioServer = app => {
     app.locals.chatrooms = [];
     const server = require('http').Server(app);
 
-    const io = require('socket.io')(server);
+    const io = require('socket.io').listen(server);
     io.use((socket, next) => {
         require('./session')(socket.request, {}, next);
     });
