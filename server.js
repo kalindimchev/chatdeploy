e1f4b22418f48ab60fe95df8ed5233a84a486837
kalindimchev <kalin.dimchev@gmail.com>
2017-09-21 17:14:51 +0300

    try fix

--- server.js
+++ server.js
@@ -4,17 +4,31 @@ const express = require('express');
 const app = require('express')();
 const chatcat = require('./app');
 const passport = require('passport');
-var io;
-var server;
 
 app.set('port', process.env.PORT || 3000);
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
-app.use(chatcat.session);
+app.use(require('./app/session'));
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use('/', chatcat.router);
+app.use('/', require('./app/routes')());
 
-chatcat.ioServer(app).listen(app.get('port'));
+app.locals.chatrooms = [];
+
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+io.use((socket, next) => {
+    require('./app/session')(socket.request, {}, next);
+});
+require('./app/socket')(io, app);
+require('./app/auth')();
+
+server.listen(app.get('port'), () => {
+    console.log(app.get('port'));
+    console.log(process.env.PORT);
+    console.log('ChatCAT Running on Port: ' + app.get('port'));
+});
+
