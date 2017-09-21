56e8ed4ac08e880f27a98a813e75ba3d2191db18
kalindimchev <kalin.dimchev@gmail.com>
2017-09-21 12:00:50 +0300

    try fix socket.io

--- app/index.js
+++ app/index.js
@@ -7,15 +7,17 @@ require('./auth')();
 let ioServer = app => {
     app.locals.chatrooms = [];
   
+    const server = require('http').Server(app);
     const io = require('socket.io').listen(server);
     io.use((socket, next) => {
         require('./session')(socket.request, {}, next);
     });
     require('./socket')(io, app);
     
-    const server = app.listen(app.get('port'), () => {
-        console.log(app.get('port'));
-    });
+    return server;
+    // const server = app.listen(app.get('port'), () => {
+    //     console.log(app.get('port'));
+    // });
 }
 
 module.exports = {
