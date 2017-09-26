41d7c0fb3dbf183f1748b55508fe2695f36d0e77
kalindimchev <kalin.dimchev@gmail.com>
2017-09-26 14:43:16 +0300

    app

--- app/index.js
+++ app/index.js
@@ -1,24 +1,24 @@
-'user strict'
+'use strict';
 
-//Social authentication
+const config = require('./config');
+
+// Social Authentication Logic
 require('./auth')();
 
-//Create IO Server instance
+// Create an IO Server instance
 let ioServer = app => {
-    app.locals.chatrooms = [];
-
-    const server = require('http').Server(app);
-    const io = require('socket.io')(server);
-
-    io.use((socket, next) => {
-        require('./session')(socket.request, {}, next);
-    });
-    require('./socket')(io, app);
-    return server;
+	app.locals.chatrooms = [];
+	const server = require('http').Server(app);
+	const io = require('socket.io')(server);
+	io.use((socket, next) => {
+		require('./session')(socket.request, {}, next);
+	});
+	require('./socket')(io, app);
+	return server;
 }
 
 module.exports = {
-    router: require('./routes')(),
-    session: require('./session'),
-    ioServer
+	router: require('./routes')(),
+	session: require('./session'),
+	ioServer
 }
