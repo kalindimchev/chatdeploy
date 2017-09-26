720408a7c3687ad984f4d3028862d2ec6197e155
kalindimchev <kalin.dimchev@gmail.com>
2017-09-26 14:02:41 +0300

    auth

--- app/auth/index.js
+++ app/auth/index.js
@@ -1,38 +1,39 @@
 'use strict';
-
 const passport = require('passport');
 const config = require('../config');
+const h = require('../helpers');
 const FacebookStrategy = require('passport-facebook').Strategy;
 const TwitterStrategy = require('passport-twitter').Strategy;
-const helper = require('../helpers');
 
 module.exports = () => {
-    passport.serializeUser((user, done) => {
-        done(null, user.id);
-    });
+	passport.serializeUser((user, done) => {
+		done(null, user.id);
+	});
 
-    passport.deserializeUser((id, done) => {
-        //Find the user by _id
-        helper.findById(id)
-            .then(user => done(null, user))
-            .catch(error => console.log('Error when deserializing the user'));
-    })
-    
-    let authProcessor = (accessToken, refreshToken, profile, done) => {
-        helper.findOne(profile.id)
-            .then(result => {
-                if (result) {
-                    console.log(result)
-                    done(null, result)
-                } else {
-                    //Create new user and return
-                    helper.createNewUser(profile)
-                        .then(newChatUser => done(null, newChatUser))
-                        .catch(error => console.log('Error when create new user'));
-                }
-            })
-    }
+	passport.deserializeUser((id, done) => {
+		// Find the user using the _id
+		h.findById(id)
+			.then(user => done(null, user))
+			.catch(error => console.log('error', 'Error when deserializing the user: ' + error));
+	});
 
-    passport.use(new FacebookStrategy(config.fb, authProcessor));
-    passport.use(new TwitterStrategy(config.twitter, authProcessor));
+	let authProcessor = (accessToken, refreshToken, profile, done) => {
+		// Find a user in the local db using profile.id
+		// If the user is found, return the user data using the done()
+		// If the user is not found, create one in the local db and return
+		h.findOne(profile.id)
+			.then(result => {
+				if(result) {
+					done(null, result);
+				} else {
+					// Create a new user and return
+					h.createNewUser(profile)
+						.then(newChatUser => done(null, newChatUser))
+						.catch(error => console.log('error', 'Error when creating new user: ' + error));
+				}
+			});
+	}
+
+	passport.use(new FacebookStrategy(config.fb, authProcessor));
+	passport.use(new TwitterStrategy(config.twitter, authProcessor));
 }
