4e0ed488e562ab694e304797fad4a453cfb6f530
kalindimchev <kalin.dimchev@gmail.com>
2017-09-26 13:46:24 +0300

    server.js, package.json

--- server.js
+++ server.js
@@ -1,23 +1,27 @@
 'use strict';
-
 const express = require('express');
 const app = express();
-const chatcat = require('./app');
+const chatCat = require('./app');
 const passport = require('passport');
 
 app.set('port', process.env.PORT || 3000);
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
-app.use(chatcat.session);
+app.use(chatCat.session);
 app.use(passport.initialize());
 app.use(passport.session());
+app.use(require('morgan')('combined', {
+	stream: {
+		write: message => {
+			// Write to logs
+			chatCat.logger.log('info', message);
+		}
+	}
+}));
 
-app.use('/', chatcat.router);
+app.use('/', chatCat.router);
 
-chatcat.ioServer(app).listen(app.get('get'), () => {
-    console.log(app.get('port'));
-    console.log(process.env.PORT);
-    console.log('ChatCAT Running on Port: ' + app.get('port'));
+chatCat.ioServer(app).listen(app.get('port'), () => {
+	console.log('ChatCAT Running on Port: ', app.get('port'));
 });
-
